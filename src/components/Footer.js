95a2d26c6741e97300cf05288b5b2c5ba57272f5
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2019-12-17 18:51:06 +0530

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './css/Footer.css'
+
+function Footer(props) {
+    return (
+        <footer className="footer">
+            <div className='row'>
+                <div className='col-12 col-sm-6 offset-sm-3'>
+                    <p className='copyright'>&#x24B8; Copyright 2019. All Rights Reserved.</p>
+                </div>
+            </div>
+        </footer>
+
+    )
+}
+
+export default Footer
